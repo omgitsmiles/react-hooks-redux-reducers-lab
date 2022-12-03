a46a8c67b0ce929a7fe48feaca9d2b557af0b778
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-12-03 14:02:28 -0500

    commit tree e928b1f9f90e94091cb7c131abff733abd0f9c59

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,13 @@
-export function manageFriends(state, action) {
-  // your code here
+export function manageFriends(state = { friends: [] }, action) {
+  if (action.type === "friends/add") {
+    return {
+      friends: [...state.friends, action.payload]
+    }
+  } else if (action.type === "friends/remove") {
+    return { 
+      friends: state.friends.filter(friend => friend.id !== action.payload)
+    } 
+  }
+  else 
+  return state
 }
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,8 @@
 const initialState = { numberOfPresents: 0 };
 
 export function managePresents(state = initialState, action) {
-  // your code here
+  if (action.type === "presents/increase") {
+    return { numberOfPresents: state.numberOfPresents + 1} 
+  } else
+  return state
 }
